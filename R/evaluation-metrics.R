110b9c3e8b5841bc5caaada97f4cd1a81438281a
riccardo-df <difrancesco.riccardo96@gmail.com>
2023-05-04 12:14:18 +0200

    Mean Absolute Error

--- R/evaluation-metrics.R
+++ R/evaluation-metrics.R
@@ -6,7 +6,7 @@
 #' @param predictions Predictions.
 #' @param use.true If \code{TRUE}, then the program treats \code{y} as a matrix of true probabilities.
 #' 
-#' @return The MSE, the RPS, or the classification error of the method.
+#' @return The MSE, the MAE, the RPS, or the CE of the method.
 #' 
 #' @examples 
 #' ## Load data from orf package.
@@ -40,19 +40,25 @@
 #' 
 #' @md
 #' @details 
-#' ## MSE and RPS
-#' When calling \code{\link{mean_squared_error}} or \code{\link{mean_ranked_score}}, \code{predictions} must be a matrix of predicted 
-#' class probabilities, with as many rows as observations in \code{y} and as many columns as classes of \code{y}.\cr
+#' ## MSE, MAE, and RPS
+#' When calling one of \code{\link{mean_squared_error}}, \code{\link{mean_absolute_error}}, or \code{\link{mean_ranked_score}}, 
+#' \code{predictions} must be a matrix of predicted  class probabilities, with as many rows as observations in \code{y} and as 
+#' many columns as classes of \code{y}.\cr
 #' 
-#' If \code{use.true == FALSE}, the mean squared error (MSE) and the mean ranked probability score (RPS) are computed as follows:
+#' If \code{use.true == FALSE}, the mean squared error (MSE), the mean absolute error (MAE), and the mean ranked probability score 
+#' (RPS) are computed as follows:
 #' 
 #' \deqn{MSE = \frac{1}{n} \sum_{i = 1}^n \sum_{m = 1}^M (1 (Y_i = m) - \hat{p}_m (x))^2}
 #' 
+#' \deqn{MAE = \frac{1}{n} \sum_{i = 1}^n \sum_{m = 1}^M |1 (Y_i = m) - \hat{p}_m (x)|}
+#' 
 #' \deqn{RPS = \frac{1}{n} \sum_{i = 1}^n \frac{1}{M - 1} \sum_{m = 1}^M (1 (Y_i \leq m) - \hat{p}_m^* (x))^2}
 #' 
-#' If \code{use.true == TRUE}, the MSE and the RPS are computed as follows (useful for simulation studies):
+#' If \code{use.true == TRUE}, the MSE, the MAE, and the RPS are computed as follows (useful for simulation studies):
 #' 
 #' \deqn{MSE = \frac{1}{n} \sum_{i = 1}^n \sum_{m = 1}^M (p_m (x) - \hat{p}_m (x))^2}
+#' 
+#' \deqn{MSE = \frac{1}{n} \sum_{i = 1}^n \sum_{m = 1}^M |p_m (x) - \hat{p}_m (x)|}
 #' 
 #' \deqn{RPS = \frac{1}{n} \sum_{i = 1}^n \frac{1}{M - 1} \sum_{m = 1}^M (p_m^* (x) - \hat{p}_m^* (x))^2}
 #' 
@@ -65,7 +71,7 @@
 #' ## Classification error
 #' When calling \code{\link{classification_error}}, \code{predictions} must be a vector of predicted class labels.\cr
 #'  
-#' Classification error is computed as follows:
+#' Classification error (CE) is computed as follows:
 #' 
 #' \deqn{CE = \frac{1}{n} \sum_{i = 1}^n 1 (Y_i \neq \hat{Y}_i)}
 #' 
@@ -98,6 +104,36 @@ mean_squared_error <- function(y, predictions, use.true = FALSE) { # Taken from 
   
   ## Output.
   return(mse)
+}
+
+
+#' Mean Absolute Error
+#'
+#' @rdname mean_squared_error
+#'
+#' @export
+mean_absolute_error <- function(y, predictions, use.true = FALSE) {
+  ## Handling inputs and checks.
+  if (!(use.true %in% c(TRUE, FALSE))) stop("'use.true' must be logical.", call. = FALSE)
+  if (use.true == TRUE & is.null(dim(y)) | use.true == FALSE & !is.null(dim(y))) stop("Combination of 'use.true' and 'y' is non-sensical.")
+  n <- dim(predictions)[1]
+  n.classes <- dim(predictions)[2]
+  
+  ## Generating matrix for comparison. Either a matrix of indicator variables, or the true probability matrix.
+  if (use.true == FALSE) {
+    indicator_mat <- matrix(0, nrow = n, ncol = n.classes)
+    for (i in seq_len(n)) {
+      indicator_mat[i, y[i]] <- 1
+    }
+  } else if (use.true == TRUE) {
+    indicator_mat <- y
+  }
+  
+  ## MSE.
+  mae <- mean(rowSums(apply((indicator_mat - predictions), 2, function(x) {abs(x)})))
+  
+  ## Output.
+  return(mae)
 }
 
 
